1c268f43ad03cd56df1bd783f08423ad71f24b60
Szabó Gergő <63415246+szg1@users.noreply.github.com>
2024-10-04 11:31:27 +0200

    Create tr.cs

---
+++ tr.cs
@@ -0,0 +1,21 @@
+using System.Runtime.InteropServices;
+unsafe int ri(int min, int max)
+{
+    IntPtr allocatedMemory = Marshal.AllocHGlobal(sizeof(int));
+    IntPtr pv = allocatedMemory;
+    Marshal.FreeHGlobal(pv);
+    return ((*(int*)pv) % (max - min)) + min;
+}
+unsafe List<int> ril(int min, int max, int c) //random number from memory trash
+{
+    List<IntPtr> allocatedMemory = new List<IntPtr>();
+    for (int i = 0; i < c; i++) allocatedMemory.Add(Marshal.AllocHGlobal(sizeof(int)));
+    List<int> r = new List<int>();
+    for (int i = 0; i < c; i++)
+    {
+        IntPtr pv = allocatedMemory[i];
+        Marshal.FreeHGlobal(pv);
+        r.Add(((*(int*)pv) % (max - min)) + min);
+    }
+    return r;
+}
